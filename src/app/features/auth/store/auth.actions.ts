f5b482d3cd9a02376a16937d35a486729f231ba8
george-asiedu <asiedug41@gmail.com>
2026-05-23 19:55:28 +0000

    auth actions defined

---
+++ src/app/features/auth/store/auth.actions.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import {
+  AuthState,
+  LoginModel,
+  RegisterModel,
+  ResetPasswordModel,
+  VerifyEmailModel,
+  VerifyMfaModel,
+} from '../../../core/models/auth.model';
+
+export const authActions = createActionGroup({
+  source: 'Auth',
+  events: {
+    Register: props<{ model: RegisterModel }>(),
+    RegisterSuccess: props<{ registered: any }>(),
+
+    Login: props<{ model: LoginModel }>(),
+    LoginSuccess: props<{ loggedIn: any }>(),
+
+    VerifyEmail: props<{ model: VerifyEmailModel }>(),
+    VerifyEmailSuccess: props<{ response: any }>(),
+
+    VerifyMfa: props<{ model: VerifyMfaModel }>(),
+    VerifyMfaSuccess: props<{ response: any }>(),
+
+    RequestPasswordReset: props<{ email: string }>(),
+    RequestPasswordResetSuccess: props<{ response: any }>(),
+
+    ResetPassword: props<{ model: ResetPasswordModel; token: string }>(),
+    ResetPasswordSuccess: props<{ response: any }>(),
+
+    VerifyToken: props<{ token: string }>(),
+    VerifyTokenSuccess: props<{ response: any }>(),
+
+    RefreshToken: emptyProps(),
+    RefreshTokenSuccess: props<{ refreshToken: any }>(),
+
+    AuthError: props<{ error: string }>(),
+    GetStorage: props<AuthState>(),
+    Logout: emptyProps(),
+  },
+});
