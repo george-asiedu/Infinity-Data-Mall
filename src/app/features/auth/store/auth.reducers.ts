d4d9691d0ac1c857ea506e0e749521fc73cbb58d
george-asiedu <asiedug41@gmail.com>
2026-05-23 19:55:12 +0000

    auth reducer defined

---
+++ src/app/features/auth/store/auth.reducers.ts
@@ -0,0 +1,89 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { AuthState } from '../../../core/models/auth.model';
+import { authActions } from './auth.actions';
+
+export const initialState: AuthState = {
+  isLoading: false,
+  loggedIn: null,
+  registered: null,
+  refreshToken: null,
+  error: null,
+};
+
+export const authFeature = createFeature({
+  name: 'Auth',
+  reducer: createReducer(
+    initialState,
+    on(
+      authActions.register,
+      authActions.login,
+      authActions.verifyEmail,
+      authActions.verifyMfa,
+      authActions.requestPasswordReset,
+      authActions.resetPassword,
+      authActions.verifyToken,
+      authActions.refreshToken,
+      (state) => ({
+        ...state,
+        isLoading: true,
+      }),
+    ),
+    on(authActions.registerSuccess, (state, { registered }) => ({
+      ...state,
+      isLoading: false,
+      registered,
+    })),
+    on(authActions.loginSuccess, (state, { loggedIn }) => ({
+      ...state,
+      isLoading: false,
+      loggedIn,
+    })),
+    on(authActions.verifyEmailSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      response,
+    })),
+    on(authActions.verifyMfaSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      response,
+    })),
+    on(authActions.requestPasswordResetSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      response,
+    })),
+    on(authActions.resetPasswordSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      response,
+    })),
+    on(authActions.verifyTokenSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      response,
+    })),
+    on(authActions.refreshTokenSuccess, (state, { refreshToken }) => ({
+      ...state,
+      isLoading: false,
+      refreshToken,
+      error: null,
+    })),
+    on(authActions.authError, (state, { error }) => ({
+      ...state,
+      isLoading: false,
+      error,
+    })),
+    on(authActions.logout, (state) => ({
+      ...state,
+      loggedIn: null,
+      refreshToken: null,
+      error: null,
+    })),
+    on(authActions.getStorage, (state, { loggedIn, refreshToken }) => ({
+      ...state,
+      loggedIn: loggedIn,
+      refreshToken: refreshToken,
+    })),
+  ),
+});
