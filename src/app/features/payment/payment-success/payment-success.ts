7d404a3045081cd4a84502e9730ac25b48e4b632
george-asiedu <asiedug41@gmail.com>
2026-05-27 15:30:43 +0000

    payment success component created

---
+++ src/app/features/payment/payment-success/payment-success.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Button } from '../../../shared/ui/button/button';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { delay } from 'rxjs';
+import { authActions } from '../../auth/store/auth.actions';
+import { selectError } from '../../auth/store/auth.selectors';
+
+@Component({
+  selector: 'app-payment-success',
+  imports: [CommonModule, Button],
+  templateUrl: './payment-success.html',
+  styleUrl: './payment-success.css',
+})
+export class PaymentSuccess implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  private readonly store = inject(Store);
+
+  protected readonly isVerifying = signal<boolean>(true);
+  protected readonly verificationFailed = signal<boolean>(false);
+  private readonly storeError = this.store.selectSignal(selectError);
+
+  ngOnInit(): void {
+    this.extractAndVerifyPayment();
+  }
+
+  private async extractAndVerifyPayment(): Promise<void> {
+    const reference = this.route.snapshot.queryParams['reference'];
+
+    if (!reference) {
+      this.isVerifying.set(false);
+      this.verificationFailed.set(true);
+      return;
+    }
+
+    this.store.dispatch(authActions.verifyPayment({ reference }));
+
+    await delay(2500);
+
+    if (this.storeError()) {
+      this.verificationFailed.set(true);
+    }
+    this.isVerifying.set(false);
+  }
+
+  protected goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+}
