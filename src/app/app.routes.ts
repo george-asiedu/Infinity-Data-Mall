63ad5f5a0c066ae1fc7c80454b32a0dc16f21efe
george-asiedu <asiedug41@gmail.com>
2026-05-27 13:09:00 +0000

    added verify email route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,9 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./features/auth/register/register').then((m) => m.Register),
   },
+  {
+    path: 'verify-email',
+    loadComponent: () =>
+      import('./features/auth/verify-email/verify-email').then((m) => m.VerifyEmail),
+  },
 ];
