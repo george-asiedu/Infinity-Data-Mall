90180b01ce465e89d48e174b759b2a0bbaa944b9
george-asiedu <asiedug41@gmail.com>
2026-06-03 14:59:03 +0000

    payment reducer created

---
+++ src/app/features/payment/store/payment.reducers.ts
@@ -0,0 +1,56 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { PaymentState } from '../../../core/models/payment.model';
+import { paymentActions } from './payment.actions';
+
+export const initialState: PaymentState = {
+  isLoading: false,
+  error: null,
+  reference: null,
+  setup: null,
+};
+
+export const paymentFeature = createFeature({
+  name: 'Payment',
+  reducer: createReducer(
+    initialState,
+    on(
+      paymentActions.initializeTransaction,
+      paymentActions.verifyBankAccount,
+      paymentActions.completeSetup,
+      paymentActions.verifyPaymentTransaction,
+      (state) => ({
+        ...state,
+        isLoading: true,
+        error: null,
+      }),
+    ),
+    on(paymentActions.initializeTransactionSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      reference: response.data?.reference || null,
+    })),
+    on(paymentActions.verifyBankAccountSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      response,
+    })),
+    on(paymentActions.completeSetupSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      setup: response.data || response,
+    })),
+    on(paymentActions.verifyPaymentTransactionSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      reference: response.data?.reference || null,
+    })),
+    on(paymentActions.paymentError, (state, { error }) => ({
+      ...state,
+      isLoading: false,
+      error,
+    })),
+    on(paymentActions.clearPaymentState, () => ({
+      ...initialState,
+    })),
+  ),
+});
