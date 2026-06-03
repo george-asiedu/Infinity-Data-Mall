fe7794bab97cfdb380551df3ed63495122e05442
george-asiedu <asiedug41@gmail.com>
2026-06-03 14:57:18 +0000

    added payment models

---
+++ src/app/core/models/payment.model.ts
@@ -0,0 +1,23 @@
+export interface VerifyBankAccountModel {
+  accountNumber: string;
+  bankCode: string;
+}
+
+export interface InitializePaymentModel {
+  email: string;
+  amount: number;
+}
+
+export interface CompleteSetupModel {
+  businessName: string;
+  bankCode: string;
+  accountNumber: string;
+  vendorApiKey: string;
+}
+
+export interface PaymentState {
+  isLoading: boolean;
+  error: string | null;
+  reference: string | null;
+  setup: unknown | null;
+}
