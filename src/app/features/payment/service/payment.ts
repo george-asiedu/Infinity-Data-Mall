8923a60995f6af579ce6d6c772a1f38b04626861
george-asiedu <asiedug41@gmail.com>
2026-06-03 14:58:29 +0000

    payment service created

---
+++ src/app/features/payment/service/payment.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import {
+  CompleteSetupModel,
+  InitializePaymentModel,
+  VerifyBankAccountModel,
+} from '../../../core/models/payment.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Payment {
+  private api = environment.apiUrl;
+  private http = inject(HttpClient);
+
+  public verifyPaymentTransaction(reference: string) {
+    return this.http.get(`${this.api}payment/verify/${reference}`);
+  }
+
+  public initializeTransaction(model: InitializePaymentModel) {
+    return this.http.post(`${this.api}payment/initialize`, model);
+  }
+
+  public verifyBankAccount(model: VerifyBankAccountModel) {
+    return this.http.get(`${this.api}payment/verify-bank`, {
+      params: { accountNumber: model.accountNumber, bankCode: model.bankCode },
+    });
+  }
+
+  public completeSetup(model: CompleteSetupModel) {
+    return this.http.post(`${this.api}payment/complete-financial-setup`, model);
+  }
+}
