56f866c191aeb5a4a18122b7a7f14cb780bbcc5c
george-asiedu <asiedug41@gmail.com>
2026-06-05 13:31:28 +0000

    complete registration component created

---
+++ src/app/features/payment/complete-registration/complete-registration.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import PaystackPop from '@paystack/inline-js';
+import { Toast } from '../../../core/services/toast/toast';
+import { Store } from '@ngrx/store';
+import { paymentActions } from '../store/payment.actions';
+import { Button } from '../../../shared/ui/button/button';
+import { environment } from '../../../../environments/environment';
+
+@Component({
+  selector: 'app-complete-registration',
+  standalone: true,
+  imports: [Button],
+  templateUrl: './complete-registration.html',
+  styleUrl: './complete-registration.css',
+})
+export class CompleteRegistration implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  private readonly toast = inject(Toast);
+  private readonly store = inject(Store);
+
+  protected accessCode = '';
+  protected reference = '';
+  protected email = '';
+  protected paymentFailed = false;
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.accessCode = params['accessCode'];
+      this.reference = params['reference'];
+      this.email = params['email'];
+
+      if (this.accessCode) {
+        this.loadPaystackModal(this.accessCode, this.reference);
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
+  protected loadPaystackModal(accessCode: string, reference: string): void {
+    const paystack = new PaystackPop();
+
+    const paymentOptions = {
+      key: environment.paystackPublicKey,
+      onSuccess: (response: any) => {
+        this.store.dispatch(paymentActions.verifyPaymentTransaction({ reference }));
+        this.router.navigate(['/payment-success'], {
+          queryParams: { reference: reference || response.reference },
+        });
+      },
+      onCancel: () => {
+        this.toast.info(
+          'Payment window closed. Please complete payment to activate your dashboard.',
+        );
+        this.paymentFailed = true;
+      },
+    };
+
+    try {
+      paystack.resumeTransaction(accessCode, paymentOptions);
+    } catch (error) {
+      console.error('Paystack failed to load:', error);
+      this.paymentFailed = true;
+    }
+  }
+}
