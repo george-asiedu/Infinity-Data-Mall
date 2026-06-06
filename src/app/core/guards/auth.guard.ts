9bb984af6825612b27403651f5e4325daa368c73
george-asiedu <asiedug41@gmail.com>
2026-06-06 12:44:54 +0000

    added google route login accesstokens

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,7 +1,7 @@
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { inject } from '@angular/core';
-import { AuthState } from '../models/auth.model';
+import { AuthState, User, VerifyMfaResponse } from '../models/auth.model';
 import { Toast } from '../services/toast/toast';
 import { authActions } from '../../features/auth/store/auth.actions';
 import { selectAccessToken } from '../../features/auth/store/auth.selectors';
@@ -18,9 +18,28 @@ const handleUnauthorizedAccess = (store: Store<AuthState>, toast: Toast) => {
   store.dispatch(authActions.logout());
 };
 
-const checkAuth = () => {
+const checkAuth = (route: ActivatedRouteSnapshot) => {
+  const urlAccessToken = route.queryParamMap.get('access_token');
+  const urlRefreshToken = route.queryParamMap.get('refresh_token');
+
   const { store, toast } = getDependencies();
   const accessToken = store.selectSignal(selectAccessToken);
+
+  if (urlAccessToken && urlRefreshToken) {
+    const oauthPayload: VerifyMfaResponse = {
+      message: 'Successfully authenticated via Google OAuth',
+      data: {
+        user: {} as User,
+        token: {
+          accessToken: urlAccessToken,
+          refreshToken: urlRefreshToken,
+        },
+      },
+    };
+    store.dispatch(authActions.verifyMfaSuccess({ loggedIn: oauthPayload }));
+
+    return true;
+  }
 
   if (!accessToken()) {
     handleUnauthorizedAccess(store, toast);
@@ -30,6 +49,6 @@ const checkAuth = () => {
   return true;
 };
 
-export const authGuard: CanActivateFn = () => {
-  return checkAuth();
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
+  return checkAuth(route);
 };
