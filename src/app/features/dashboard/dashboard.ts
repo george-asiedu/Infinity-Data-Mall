8d9c5c1989d5bb4c6bbe5349e0ab1e9a731641d8
george-asiedu <asiedug41@gmail.com>
2026-06-06 13:58:54 +0000

    removed console logs and added toast notifications

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Toast } from '../../core/services/toast/toast';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,11 +11,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class Dashboard implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly router = inject(Router);
+  private toast = inject(Toast);
 
   ngOnInit(): void {
     const hashTokens = this.route.snapshot.queryParamMap.has('access_token');
 
     if (hashTokens) {
+      this.toast.success('Successfully authenticated via Google');
       this.router.navigate([], {
         queryParams: { access_token: null, refresh_token: null, is_new: null },
         queryParamsHandling: 'merge',
