80a2289ef90f2ec260f800b3600562e7629cde44
george-asiedu <asiedug41@gmail.com>
2026-06-07 15:09:07 +0000

    added title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,24 +9,29 @@ export const routes: Routes = [
   },
   {
     path: 'verify-email',
+    title: 'Infinity Data Mall | Verify Email',
     loadComponent: () =>
       import('./features/auth/verify-email/verify-email').then((m) => m.VerifyEmail),
   },
   {
     path: 'login',
+    title: 'Infinity Data Mall | Login',
     loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
   },
   {
     path: 'mfa',
+    title: 'Infinity Data Mall | OTP',
     loadComponent: () => import('./features/auth/mfa/mfa').then((m) => m.Mfa),
   },
   {
     path: 'forgot-password',
+    title: 'Infinity Data Mall | Forgot Password',
     loadComponent: () =>
       import('./features/auth/forgot-password/forgot-password').then((m) => m.ForgotPassword),
   },
   {
     path: 'reset-password',
+    title: 'Infinity Data Mall | Reset Password',
     loadComponent: () =>
       import('./features/auth/reset-password/reset-password').then((m) => m.ResetPassword),
     canActivate: [resetPasswordGuard],
@@ -38,13 +43,22 @@ export const routes: Routes = [
   },
   {
     path: 'complete-registration',
+    title: 'Infinity Data Mall | Complete Registration',
     loadComponent: () =>
       import('./features/payment/complete-registration/complete-registration').then(
         (m) => m.CompleteRegistration,
       ),
   },
   {
+    path: 'onboarding',
+    title: 'Infinity Data Mall | Onboarding',
+    loadComponent: () =>
+      import('./features/payment/onboarding/onboarding').then((m) => m.Onboarding),
+    canActivate: [authGuard],
+  },
+  {
     path: 'dashboard',
+    title: 'Infinity Data Mall | Agent Dashboard',
     loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
     canActivate: [authGuard],
   },
