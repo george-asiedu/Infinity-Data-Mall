d083950e46770424d2c8522d2b2b2ab598a879c3
george-asiedu <asiedug41@gmail.com>
2026-06-07 20:06:00 +0000

    added filters to select component

--- src/app/shared/ui/select/select.ts
+++ src/app/shared/ui/select/select.ts
@@ -1,21 +1,25 @@
 import { Component, input, model, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
 import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-select',
-  imports: [SelectModule, FormsModule],
+  imports: [SelectModule, FormsModule, FloatLabelModule],
   templateUrl: './select.html',
   styleUrl: './select.css',
 })
 export class Select<T> {
+  public label = input<string>('');
   public value = model<T | null>(null);
   public options = input<T[]>([]);
   public optionLabel = input<string>('label');
   public optionValue = input<string>('value');
-  public placeholder = input<string>('Select an option');
+  public placeholder = input<string>('');
   public disabled = input<boolean>(false);
   public customClass = input<string>('w-full control-base');
 
+  public filter = input<boolean>(false);
+  public filterBy = input<string>('label');
   public selectionChanged = output<T>();
 }
