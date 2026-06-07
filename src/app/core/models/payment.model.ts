2f954fd82344ced4b822db5525bc53729a945ae7
george-asiedu <asiedug41@gmail.com>
2026-06-07 20:04:16 +0000

    updated payment models

--- src/app/core/models/payment.model.ts
+++ src/app/core/models/payment.model.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface VerifyBankAccountModel {
   accountNumber: string;
   bankCode: string;
@@ -19,7 +20,42 @@ export interface PaymentState {
   isLoading: boolean;
   error: string | null;
   reference: string | null;
-  setup: unknown | null;
-  banks: any[];
+  setup: any | null;
+  banks: Banks[];
   accounts: any[];
+}
+
+export interface BanksResponse {
+  status: boolean;
+  message: boolean;
+  data: Banks[];
+}
+
+export interface Banks {
+  id: number;
+  name: string;
+  slug: string;
+  code: string;
+  longcode: string;
+  gateway: any;
+  pay_with_bank: boolean;
+  supports_transfer: boolean;
+  available_for_direct_debit: boolean;
+  active: boolean;
+  country: string;
+  currency: string;
+  type: string;
+  is_deleted: boolean;
+  createdAt: string;
+  updatedAt?: string;
+}
+
+export interface VerifyAccountResponse {
+  status: boolean;
+  message: string;
+  data: {
+    account_number: string;
+    account_name: string;
+    bank_id: number;
+  };
 }
