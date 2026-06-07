ff3d5c071f77d2a152472da8d663b01a082804a1
george-asiedu <asiedug41@gmail.com>
2026-06-07 13:11:06 +0000

    refactored mfa to inlcude backup recovery

--- src/app/features/auth/mfa/mfa.ts
+++ src/app/features/auth/mfa/mfa.ts
@@ -1,13 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Button } from '../../../shared/ui/button/button';
 import { CommonModule } from '@angular/common';
 import { FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { delay } from '../../../shared/utils/helpers';
 import { Store } from '@ngrx/store';
 import { authActions } from '../store/auth.actions';
 import { Input } from '../../../shared/ui/input/input';
-import { selectMfaToken } from '../store/auth.selectors';
+import { selectMfaToken, selectRegistrationEmail } from '../store/auth.selectors';
 import { Toast } from '../../../core/services/toast/toast';
 
 @Component({
@@ -23,19 +22,30 @@ export class Mfa {
   private readonly router = inject(Router);
   private readonly toast = inject(Toast);
 
+  protected readonly isBackupMode = signal<boolean>(false);
+  protected readonly email = this.store.selectSignal(selectRegistrationEmail);
+
   protected mfaForm: FormGroup = this.fb.group({
     code: ['', [Validators.required, Validators.pattern('^[0-9]{6}$')]],
+    backupCode: ['', [Validators.required, Validators.pattern('^[A-Za-z0-9]{2}-[A-Za-z0-9]{6}$')]],
   });
 
   private readonly mfaTokenResponse = this.store.selectSignal(selectMfaToken);
 
+  protected toggleVerificationMode(): void {
+    this.isBackupMode.update((state) => !state);
+  }
+
   protected async onSubmit() {
-    if (this.mfaForm.invalid) {
-      this.mfaForm.markAllAsTouched();
+    const activeControl = this.isBackupMode()
+      ? this.getControl('backupCode')
+      : this.getControl('code');
+
+    if (!activeControl || activeControl.invalid) {
+      activeControl?.markAllAsTouched();
       return;
     }
 
-    const code = this.mfaForm.getRawValue().code;
     const loginResponse = this.mfaTokenResponse();
     const token = loginResponse?.data?.mfaToken;
 
@@ -45,15 +55,21 @@ export class Mfa {
       return;
     }
 
-    const model = {
-      mfaToken: token,
-      code: code,
-    };
-
-    this.store.dispatch(authActions.verifyMfa({ model }));
-
-    await delay();
-    this.mfaForm.reset();
+    if (this.isBackupMode()) {
+      const backupCode: string = this.mfaForm.getRawValue().backupCode.trim().toUpperCase();
+      const model = {
+        email: this.email() as string,
+        backupCode: backupCode,
+      };
+      this.store.dispatch(authActions.loginWithCode({ model }));
+    } else {
+      const code = this.mfaForm.getRawValue().code.trim();
+      const model = {
+        mfaToken: token,
+        code: code,
+      };
+      this.store.dispatch(authActions.verifyMfa({ model }));
+    }
   }
 
   protected getControl(name: string) {
