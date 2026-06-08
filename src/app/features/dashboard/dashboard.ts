835d1774d504b4ffb8ba3ee7f4f67431013d040c
george-asiedu <asiedug41@gmail.com>
2026-06-08 20:11:42 +0000

    dashboard component created

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { Toast } from '../../core/services/toast/toast';
+import { Store } from '@ngrx/store';
+import { authActions } from '../auth/store/auth.actions';
+import { CommonModule } from '@angular/common';
+import { Sidebar } from '../../shared/components/sidebar/sidebar';
+import { Header } from '../../shared/components/header/header';
+import { selectUser } from '../auth/store/auth.selectors';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule, RouterOutlet, Sidebar, Header],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
@@ -12,6 +18,16 @@ export class Dashboard implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly router = inject(Router);
   private toast = inject(Toast);
+  private store = inject(Store);
+
+  protected isSidebarOpen = signal<boolean>(false);
+  protected user = this.store.selectSignal(selectUser);
+
+  protected readonly activeSub = signal({
+    planName: 'Active · GHS 100/mo',
+    timeLeftStr: '21 days left',
+    percentageLeft: 70,
+  });
 
   ngOnInit(): void {
     const hashTokens = this.route.snapshot.queryParamMap.has('access_token');
@@ -23,5 +39,13 @@ export class Dashboard implements OnInit {
         queryParamsHandling: 'merge',
       });
     }
+  }
+
+  protected toggleMobileSidebar(): void {
+    this.isSidebarOpen.update((val) => !val);
+  }
+
+  protected handleLogoutAction(): void {
+    this.store.dispatch(authActions.logout());
   }
 }
