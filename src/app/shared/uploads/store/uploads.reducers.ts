bda7b33c242d9b463619269351a7dc8d29687440
george-asiedu <asiedug41@gmail.com>
2026-06-10 20:50:44 +0000

    upload reducer functions created

---
+++ src/app/shared/uploads/store/uploads.reducers.ts
@@ -0,0 +1,44 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { UploadState } from '../../../core/models/upload.model';
+import { uploadActions } from './uploads.actions';
+
+export const initialState: UploadState = {
+  isLoading: false,
+  error: null,
+  activeUpload: null,
+  completedAsset: null,
+  category: null,
+};
+
+export const uploadFeature = createFeature({
+  name: 'Upload',
+  reducer: createReducer(
+    initialState,
+    on(uploadActions.initiateUpload, (state, { model }) => ({
+      ...state,
+      isLoading: true,
+      category: model.category,
+    })),
+    on(uploadActions.completeUpload, (state) => ({
+      ...state,
+      isLoading: true,
+      error: null,
+    })),
+    on(uploadActions.initiateUploadSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      activeUpload: response,
+    })),
+    on(uploadActions.completeUploadSuccess, (state, { response }) => ({
+      ...state,
+      isLoading: false,
+      completedAsset: response.data,
+    })),
+    on(uploadActions.uploadError, (state, { error }) => ({
+      ...state,
+      isLoading: false,
+      error,
+    })),
+    on(uploadActions.clearUploadState, () => initialState),
+  ),
+});
