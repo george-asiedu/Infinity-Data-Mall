58d70af9b32c88eaa47478bc9b9bbd3f548a331d
george-asiedu <asiedug41@gmail.com>
2026-06-12 21:22:01 +0000

    clean up dashboard

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
-import { Toast } from '../../core/services/toast/toast';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { authActions } from '../auth/store/auth.actions';
 import { CommonModule } from '@angular/common';
@@ -14,10 +13,7 @@ import { selectUser } from '../auth/store/auth.selectors';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard implements OnInit {
-  private readonly route = inject(ActivatedRoute);
-  private readonly router = inject(Router);
-  private toast = inject(Toast);
+export class Dashboard {
   private store = inject(Store);
 
   protected isSidebarOpen = signal<boolean>(false);
@@ -28,18 +24,6 @@ export class Dashboard implements OnInit {
     timeLeftStr: '21 days left',
     percentageLeft: 70,
   });
-
-  ngOnInit(): void {
-    const hashTokens = this.route.snapshot.queryParamMap.has('access_token');
-
-    if (hashTokens) {
-      this.toast.success('Successfully authenticated via Google');
-      this.router.navigate([], {
-        queryParams: { access_token: null, refresh_token: null, is_new: null },
-        queryParamsHandling: 'merge',
-      });
-    }
-  }
 
   protected toggleMobileSidebar(): void {
     this.isSidebarOpen.update((val) => !val);
