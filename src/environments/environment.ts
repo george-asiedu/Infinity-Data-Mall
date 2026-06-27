6e2c6b09ae362f6425abe84e6fe81b480bf8c971
george-asiedu <asiedug41@gmail.com>
2026-06-27 18:02:40 +0000

    shopBaseUrl added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://infinity-data-mall.onrender.com/api/',
   paystackPublicKey: 'pk_test_f6ca4865e67618ab49872fa5855a5eb741e691a6',
+  shopBaseUrl: '',
 };
